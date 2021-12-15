bceb2bf176ffb0db2af889b980acd57ba4ed43ed
Sara Landis <saralandis5555@gmail.com>
2021-12-15 12:59:30 -0500

    server set up and folder organization

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('client/dist'));
+
+app.listen(port, () => {
+  console.log(`Thesis page listening at http://localhost:${port}`)
+})
